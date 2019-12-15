3089affe5ef62c7b12ec8f46183b959bc42dac00
SultanaJahan <sonisultana1454@gmail.com>
2019-12-15 17:25:57 -0500

    homework

---
+++ src/Homework1/homeworkJava.java
@@ -0,0 +1,19 @@
+package Homework1;
+
+public class homeworkJava {
+
+	public static void main(String[] args) {
+		// create variables for :-a. Your Name
+	     String SS="SULTANA SONIA,";
+	     // b. Your age
+	     String AG=" I am 29 years old,";
+	     //  c. your gender
+	     String GN=" Gender is FEMALE,";
+	     //d. your martial status
+	     String MS=" I am Married.";
+	     
+	     System.out.println(SS+AG+GN+MS);
+
+	}
+
+}
