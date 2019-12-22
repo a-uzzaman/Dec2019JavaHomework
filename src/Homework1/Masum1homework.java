560e80a1b860781b6e64b8c21ee0a8dcf029acdb
Masumreza1 <58913285+Masumreza1@users.noreply.github.com>
2019-12-22 12:23:36 -0500

    Completed home assigment

---
+++ src/Homework1/Masum1homework.java
@@ -0,0 +1,31 @@
+package Homework1;
+
+public class Masum1homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		The value proof of the number is variable101
+		The floating value of the variable0.546732
+		The stringstored inthe charectors variableis Any string
+		The bolean value of the variable istrue
+		101.54673 Any stringtrue*/
+		
+		
+//		next line I am printing my name
+		System.out.println("My name is Masum");
+		
+		
+		
+		
+
+	}
+
+}
+
+
+
+
+
