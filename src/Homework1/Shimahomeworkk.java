3a422b0cbe43a768596a7a318c102138b6d00ecf
shimanahar <shima.nahar19@gmail.com>
2019-12-22 12:17:27 -0500

    Created variables for class

---
+++ src/Homework1/Shimahomeworkk.java
@@ -0,0 +1,52 @@
+package Homework1;
+
+public class Shimahomeworkk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* Create a new class in homework1 package
+		///Add comments as paragraph with the homework instructions
+		//3. Create comment for each line of code explaining what you are doing
+		//4. create variables for :-
+		//   a. Your Name
+		//   b. Your age
+		//   c. your gender
+		//   d. your martial status
+		//5. print out a sentence explaining each variable.
+		//6. Push your code to github and create a pull request */
+	
+		
+		
+		System.out.println ("I am creating variables");
+		
+	    String name = "Shima"; // I am writing a string for the variable name
+	    int age = 28; // I am writing a integer for the variable age
+	    String gender ="female"; // I am writing a string for the variable gender
+	    String maritalstatus= "married"; // I am writing a string for the variable maritalstatus
+	    
+	    System.out.println ("my name"+"username");
+	    System.out.println ("my age is" +age);
+	    System.out.println ("my gender is"+"female");
+	    System.out.println ("my maritalstatus is"+"married");
+	    
+	    
+	    	
+	    
+	    
+	    	
+	    		
+	    
+	    
+	    
+		
+		
+		
+	}
+
+
+}
+
+
+
+
