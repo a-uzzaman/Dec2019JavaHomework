620db36f2f0cf7e08b2a125fde0bc267e77b2ecb
Masumreza1 <58913285+Masumreza1@users.noreply.github.com>
2019-12-22 12:41:37 -0500

    Home work is completed

--- src/Homework1/Masum1homework.java
+++ src/Homework1/Masum1homework.java
@@ -5,19 +5,53 @@ public class Masum1homework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*0. Create a branch
+1. Create a new class in homework1 package
+2. Add comments as paragraph with the homework instructions
+3. Create comment for each line of code explaining what you are doing
+4. create variables for :-
+a. Your Name
+b. Your age
+c. your gender
+d. your marital status
+5. print out a sentence explaining each variable.
+6. Push your code to github and create a pull request.*/
 		
-		/*
-		The value proof of the number is variable101
-		The floating value of the variable0.546732
-		The stringstored inthe charectors variableis Any string
-		The bolean value of the variable istrue
-		101.54673 Any stringtrue*/
+		/*int numbers;
+		float floatingNumbers;
+		String charecter;
+		boolean booleanVariable;*/
+		
+		String name,age,gender,marital;
+	
+		
+//I am creating name variable, this is my name Masum
+		 name ="Masum";
+		System.out.println(name);
+		
+//		I am creating int variable, This is my age 50
+	    age="50";
+		System.out.println(age);
 		
 		
-//		next line I am printing my name
-		System.out.println("My name is Masum");
+//		I am creating variable for gender,This is my gender M
+		gender ="M";
+		System.out.println("gender");
 		
+//		I am creating variable for marital status, marital status is Married
+		marital="Married";
+		System.out.println("marital");
 		
+//		My name, my age, my gender and my marital status
+		
+//		System.out.println("My name is: "+ s + ",  This is my age: "+ a + " , My gender is: " + c +" , Marital status:" + s1);
+		System.out.println("my name is: "+ name);
+		System.out.println("my age is: "+ age);
+		System.out.println("my gender is: "+ gender);
+		System.out.println("my marital status is: "+ marital);
+		System.out.println("completed");
+		
+//	completed assigment
 		
 		
 
